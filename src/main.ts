faa777c6cd154c5cfc0d4fc344000e9d08d87f4c
Jarno Luoma-Nirva <jln@iki.fi>
2023-09-03 19:47:47 +0300

    Basic configuration for vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,26 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
+import en from './locales/en.json'
+
+const messages = {
+  'en': en
+}
+
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
